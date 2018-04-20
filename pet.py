1304c1a057ace5ed65c513d1820930ddcf78d9b7
test <test@ip-172-31-44-246.us-east-2.compute.internal>
2018-04-20 04:35:34 +0000

    add the feature to find the nearst dog available for feature

---
+++ pet.py
@@ -0,0 +1,28 @@
+import petfinder
+
+# Instantiate the client with your credentials.
+api = petfinder.PetFinderClient(api_key='90999df88cd81af6e271a7a661ee5bf6', api_secret='57d9d3da742d84021f892c623667db77')
+
+# Query away!
+#for shelter in api.shelter_find(location='30127', count=500):
+ #   print(shelter['name'])
+
+found = 0
+# Search for pets.
+for pet in api.pet_find(
+    animal="dog", location="19104", output="basic",
+    breed="Beagle", count=4,
+):
+   
+    contact = pet['contact']
+    for key,val in contact.items():
+        if key == 'address1':
+           if val != None:
+              found = 1
+              break    
+    if found == 1:
+       break
+
+
+
+print("%s - %s - %s - %s - %s" % (pet['id'], pet['name'],pet['contact'], pet['sex'],pet['size']))
